d100e659d43c6d9e131eeb6af08dffa8ef77cb69
LefojGaming <LefojGaming@DESKTOP-VSG9DGS>
2026-05-29 06:10:08 +0800

    commit ui

--- app/(pages)/DogNameGenerator/body.tsx
+++ app/(pages)/DogNameGenerator/body.tsx
@@ -48,10 +48,8 @@ export default function Body() {
 
   const renderNames = (index: number | null) => {
     const items: JSX.Element[] = [];
-    let letterIndex = data?.data.findIndex((name) =>
-      name.title.startsWith(selectedLetter ?? "a"),
-    );
-    let startIndex = letterIndex ?? 0;
+
+    let startIndex = index ?? 0;
     let endIndex = startIndex + 7;
     let selectedIndex = startIndex + 3;
 
@@ -66,13 +64,14 @@ export default function Body() {
     }
 
     setSelectedNameIndex(selectedIndex);
+
     let count = 0;
     for (let i = startIndex; i < endIndex; i++) {
       items.push(
         <li
-          key={data?.data[i]?.id ?? `name-${data?.data[i]?.title}`}
-          className={`${styles.nameItem} ${selectedIndex === i && styles.isSelected} cursor-pointer ${styles[`nameIndex-${count}`]}`}
-          onClick={() => renderNames(i)}
+          key={data?.data[i]?.id ?? `name-${i}`}
+          className={`${styles.nameItem} ${selectedIndex === i ? styles.isSelected : ""} cursor-pointer ${styles[`nameIndex-${count}`]}`}
+          onClick={() => JumpToName(i, selectedIndex)}
         >
           {data?.data[i]?.title}
         </li>,
@@ -82,8 +81,30 @@ export default function Body() {
     setNameElements(items);
   };
 
+  const JumpToName = (targetIndex: number, currentIndex: number | null) => {
+    if (currentIndex === null || currentIndex === targetIndex) {
+      return;
+    }
+
+    const step = targetIndex > currentIndex ? 1 : -1;
+    const nextIndex = currentIndex + step;
+
+    renderNames(nextIndex);
+
+    if (nextIndex !== targetIndex) {
+      setTimeout(() => JumpToName(targetIndex, nextIndex), 200);
+    }
+  };
+
   useEffect(() => {
-    if (selectedLetter) renderNames(null);
+    if (selectedLetter) {
+      let letterIndex =
+        data?.data.findIndex((name) =>
+          name.title.startsWith(selectedLetter ?? "a"),
+        ) ?? null;
+      setSelectedNameIndex(letterIndex);
+      renderNames(letterIndex);
+    }
   }, [selectedLetter]);
 
   if (loading || error) return null;
@@ -100,54 +121,51 @@ export default function Body() {
               : ` flex flex-col items-center justify-center h-auto w-100`)
           }
         >
-          <div>
-            <i
-              className={
-                "pi pi-angle-up text-2xl items-center primary cursor-pointer"
-              }
-              style={{
-                color: "var(--primary-color)",
-                fontSize: "1.25rem",
-                fontWeight: "lighter",
-              }}
-              onClick={() => {
-                selectedNameIndex !== null && selectedNameIndex > 0
-                  ? renderNames(selectedNameIndex - 1)
-                  : null;
-              }}
+          <div className="flex flex-row items-center justify-center w-100">
+            <img
+              src={dog2.src}
+              alt="Dog"
+              className={`${isMobile && styles.mobile} ${styles.dogImage2}`}
             />
+            <ul className="w-2/5">{nameElements}</ul>
 
-            <ul>
-              {nameElements}
-
-              {/* {data?.data.map((name) => (
-                <li key={name.id}>{name.title}</li>
-              ))} */}
-            </ul>
-
-            <i
-              className={
-                "pi pi-angle-down text-2xl items-center primary cursor-pointer"
-              }
-              style={{
-                color: "var(--primary-color)",
-                fontSize: "1.25rem",
-                fontWeight: "lighter",
-              }}
-              onClick={() => {
-                selectedNameIndex !== null &&
-                selectedNameIndex < (data?.data.length ?? 0) - 1
-                  ? renderNames(selectedNameIndex + 1)
-                  : null;
-              }}
-            />
+            <div className="flex flex-col items-center justify-center w-100 w-1/5">
+              <i
+                className={
+                  "pi pi-angle-up text-2xl items-center primary cursor-pointer h-1/4" +
+                  styles.arrowIcon
+                }
+                style={{
+                  color: "var(--primary-color)",
+                  fontSize: "3.5rem",
+                  fontWeight: "200",
+                }}
+                onClick={() => {
+                  selectedNameIndex !== null && selectedNameIndex > 0
+                    ? renderNames(selectedNameIndex - 1)
+                    : null;
+                }}
+              />
+              <div className="block h-100"></div>
+              <i
+                className={
+                  "pi pi-angle-down text-2xl items-center primary cursor-pointer h-1/4" +
+                  styles.arrowIcon
+                }
+                style={{
+                  color: "var(--primary-color)",
+                  fontSize: "3.5rem",
+                  fontWeight: "200",
+                }}
+                onClick={() => {
+                  selectedNameIndex !== null &&
+                  selectedNameIndex < (data?.data.length ?? 0) - 1
+                    ? renderNames(selectedNameIndex + 1)
+                    : null;
+                }}
+              />
+            </div>
           </div>
-
-          <img
-            src={dog2.src}
-            alt="Dog"
-            className={`${isMobile && styles.mobile}`}
-          />
         </div>
       )}
     </>
