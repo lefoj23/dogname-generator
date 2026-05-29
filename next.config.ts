13970e403e68e6d84fcc2314934b8c11d56df9f3
LefojGaming <LefojGaming@DESKTOP-VSG9DGS>
2026-05-29 19:52:20 +0800

    fix build error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Enable source maps in dev to point to original .module.scss in DevTools */
+  // webpack: (config, { dev }) => {
+  //   if (dev) {
+  //     config.devtool = 'source-map';
+  //   }
+  //   return config;
+  // },
+  /* Enable source maps in production builds */
+  // productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
