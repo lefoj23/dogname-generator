5d2373951fb491465601de3113c898f09f812854
LefojGaming <LefojGaming@DESKTOP-VSG9DGS>
2026-05-29 09:29:28 +0800

    commit final logics

--- app/(pages)/DogNameGenerator/body.tsx
+++ app/(pages)/DogNameGenerator/body.tsx
@@ -1,17 +1,23 @@
 "use client";
 import dog1 from "../../assets/dog-1.png";
 import dog2 from "../../assets/dog-2.png";
+import iconLink from "../../assets/icon-link.svg";
+import iconMessenger from "../../assets/icon-messenger.svg";
+import iconTwitter from "../../assets/icon-twitter.svg";
+
 import styles from "./page.module.scss";
 import SplitText from "../../components/SplitText";
 import { useIsMobile } from "../../hooks/useIsMobile";
 import { useAppSelector } from "../../store/hooks";
 import { motion, AnimatePresence } from "motion/react";
 import { useNamesDriveData } from "../../hooks/useNamesDriveData";
+import { useCategoriesDriveData } from "../../hooks/useCategoriesDriveData";
 import { JSX, useState, useEffect } from "react";
-import { INamesResponse } from "../../models/names";
+import { INamesData, INamesResponse } from "../../models/names";
 
 export default function Body() {
-  const { data } = useNamesDriveData();
+  const { data: namesData } = useNamesDriveData();
+  const { data: categoriesData } = useCategoriesDriveData();
 
   const isMobile = useIsMobile();
   const loading = useAppSelector((state) => state.letters.isLoading);
@@ -29,7 +35,14 @@ export default function Body() {
     null,
   );
   const [nameElements, setNameElements] = useState<JSX.Element[]>([]);
-  const [loadedNames, setLoadedNames] = useState<INamesResponse | null>(data);
+  const [loadedNames, setLoadedNames] = useState<INamesResponse | null>(
+    namesData,
+  );
+  const [maxIndexNumber, setMaxIndexNumber] = useState<number>(7);
+  const [centerIndexNumber, setCenterIndexNumber] = useState<number>(3);
+
+  const [displayDetailedView, setDisplayDetailedView] =
+    useState<boolean>(false);
 
   const LandingPage = () => {
     return (
@@ -58,16 +71,19 @@ export default function Body() {
     const items: JSX.Element[] = [];
 
     let startIndex = index ?? 0;
-    let endIndex = startIndex + 7;
-    let selectedIndex = startIndex + 3;
+    let endIndex = startIndex + maxIndexNumber;
+    let selectedIndex = startIndex + centerIndexNumber;
 
     if (index != null && typeof index === "number") {
       selectedIndex = index;
-      startIndex = Math.max(0, selectedIndex - 3);
-      endIndex = Math.min(startIndex + 7, loadedNames?.data.length ?? 0);
+      startIndex = Math.max(0, selectedIndex - centerIndexNumber);
+      endIndex = Math.min(
+        startIndex + maxIndexNumber,
+        loadedNames?.data.length ?? 0,
+      );
 
-      if (endIndex - startIndex < 7) {
-        startIndex = Math.max(0, endIndex - 7);
+      if (endIndex - startIndex < maxIndexNumber) {
+        startIndex = Math.max(0, endIndex - maxIndexNumber);
       }
     }
 
@@ -78,8 +94,18 @@ export default function Body() {
       items.push(
         <li
           key={loadedNames?.data[i]?.id ?? `name-${i}`}
-          className={`${styles.nameItem} ${selectedIndex === i ? styles.isSelected : ""} cursor-pointer ${styles[`nameIndex-${count}`]}`}
-          onClick={() => JumpToName(i, selectedIndex)}
+          className={`${styles.nameItem} ${selectedIndex === i ? styles.isSelected : ""} cursor-pointer ${styles[`nameIndex-${count}`]} ${displayDetailedView ? styles.detailedView : ""}`}
+          onClick={() => {
+            selectedIndex === i
+              ? !displayDetailedView
+                ? (setDisplayDetailedView(true),
+                  setMaxIndexNumber(11),
+                  setCenterIndexNumber(5))
+                : (setDisplayDetailedView(false),
+                  setMaxIndexNumber(7),
+                  setCenterIndexNumber(3))
+              : JumpToName(i, selectedIndex);
+          }}
         >
           {loadedNames?.data[i]?.title}
         </li>,
@@ -104,20 +130,87 @@ export default function Body() {
     }
   };
 
+  const renderDetailedView = () => {
+    const nameData = loadedNames?.data[selectedNameIndex ?? 0];
+
+    let categoryLabels = categoriesData?.filterGroups
+      .filter((group) =>
+        nameData?.categories.some((catId) => group.categoryIds.includes(catId)),
+      )
+      .map((group) => group.label)
+      .join(" - ");
+
+    let relatedNames = namesData?.data
+      .filter((name) =>
+        nameData?.categories.every((catId) => name.categories.includes(catId)),
+      )
+      .map((names) => names.title)
+      .join(" - ");
+
+    console.log(relatedNames);
+
+    return (
+      <>
+        {categoryLabels && (
+          <>
+            <div className="flex flex-row gap-2">
+              {nameData?.gender.includes("M") && (
+                <i className={"pi pi-mars " + `${styles.genderIcon}`} />
+              )}
+              {nameData?.gender.includes("F") && (
+                <i className={"pi pi-venus " + `${styles.genderIcon}`} />
+              )}
+
+              <h2 className={`${styles.categoryLabels}`}>{categoryLabels}</h2>
+            </div>
+          </>
+        )}
+
+        <div
+          className={styles.definitionDiv}
+          dangerouslySetInnerHTML={{ __html: nameData?.definition as string }}
+        />
+
+        <div className={styles.footer}>
+          <span>Related Name</span>
+          <h3 className={`${styles.relatedNames}`}>{relatedNames}</h3>
+
+          <div className={`${styles.footerIcons} flex flex-row gap-1`}>
+            <img
+              src={iconLink.src}
+              alt="link"
+              className={`${isMobile && styles.mobile}`}
+            />
+            <img
+              src={iconTwitter.src}
+              alt="Twitter"
+              className={`${isMobile && styles.mobile}`}
+            />
+            <img
+              src={iconMessenger.src}
+              alt="Messenger"
+              className={`${isMobile && styles.mobile}`}
+            />
+          </div>
+        </div>
+      </>
+    );
+  };
+
   useEffect(() => {
     let letterIndex =
       loadedNames?.data.findIndex((name) =>
         name.title.startsWith(selectedLetter ?? "a"),
       ) ?? null;
-    letterIndex = letterIndex === 0 ? 3 : letterIndex;
+    letterIndex = letterIndex === 0 ? centerIndexNumber : letterIndex;
     setSelectedNameIndex(letterIndex);
     renderNames(letterIndex);
-  }, [selectedLetter, loadedNames, data]);
+  }, [selectedLetter, loadedNames, namesData, maxIndexNumber]);
 
   useEffect(() => {
     setNameElements([]);
 
-    let filteredNames = data?.data;
+    let filteredNames = namesData?.data;
 
     if (selectedGender && selectedGender !== "Both") {
       filteredNames = filteredNames?.filter((name) =>
@@ -134,7 +227,7 @@ export default function Body() {
     setLoadedNames({ data: filteredNames ?? [] });
     setSelectedNameIndex(null);
     renderNames(null);
-  }, [selectedGender, data, selectedCategoryIds]);
+  }, [selectedGender, namesData, selectedCategoryIds, maxIndexNumber]);
 
   if (loading || error) return null;
   return (
@@ -152,19 +245,25 @@ export default function Body() {
               : ` flex flex-col items-center justify-center h-auto w-100`)
           }
         >
-          <div className="flex flex-row items-center justify-center w-100">
-            <img
-              src={dog2.src}
-              alt="Dog"
-              className={`${isMobile && styles.mobile} ${styles.dogImage2}`}
-            />
+          <div className="flex flex-row items-center justify-center w-full">
+            {!displayDetailedView && (
+              <>
+                <img
+                  src={dog2.src}
+                  alt="Dog"
+                  className={`${isMobile && styles.mobile} ${styles.dogImage2}`}
+                />
+                <ul className="w-2/5">{nameElements}</ul>
+              </>
+            )}
 
-            <ul className="w-2/5">{nameElements}</ul>
             {loadedNames?.data && loadedNames?.data.length > 0 ? (
-              <div className="flex flex-col items-center justify-center w-100 w-1/5">
+              <div
+                className={`${styles.arrowDiv} flex flex-col w-100 w-1/5 items-center justify-around`}
+              >
                 <i
                   className={
-                    "pi pi-angle-up text-2xl items-center primary cursor-pointer h-1/4" +
+                    "pi pi-angle-up text-2xl items-center primary cursor-pointer h-1/4 " +
                     styles.arrowIcon
                   }
                   style={{
@@ -203,6 +302,20 @@ export default function Body() {
                 className={`text-2sm font-bold ${isMobile && styles.mobile} ${styles.noResults}`}
               />
             )}
+
+            {displayDetailedView &&
+            loadedNames?.data[selectedNameIndex ?? 0] ? (
+              <>
+                <ul className={`${styles.detailedViewUl} items-left text-left`}>
+                  {nameElements}
+                </ul>
+                <div
+                  className={`${styles.detailedViewDiv} flex flex-col items-left`}
+                >
+                  {renderDetailedView()}
+                </div>
+              </>
+            ) : null}
           </div>
         </div>
       )}
